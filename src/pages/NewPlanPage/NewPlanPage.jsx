c4efd7edc10f3b9a5cf6655ea5b71e29ee88b7cb
vsalter <vsalter1@student.gsu.edu>
2022-10-12 11:13:35 -0400

    updated the practice date, start time, and end time input types to date, time and time respectively

--- src/pages/NewPlanPage/NewPlanPage.jsx
+++ src/pages/NewPlanPage/NewPlanPage.jsx
@@ -17,11 +17,11 @@ export default function NewPlanPage() {
             <label>Practice Name</label>
             <input type="text" name="name" value={plan.name} required />
             <label>Practice Date</label>
-            <input type="text" name="date" value={plan.date}required />
+            <input type="date" name="date" value={plan.date} required />
             <label>Start Time</label>
-            <input type="text" name="startTime" value={plan.startTime}required />
+            <input type="time" name="startTime" value={plan.startTime} required />
             <label>End Time</label>
-            <input type="text" name="endTime" value={plan.endTime} required />
+            <input type="time" name="endTime" value={plan.endTime} required />
             <button type="submit">Create Practice Plan</button>
           </form>
         </div>
