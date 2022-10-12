6b06cc1b44e5bce37cae470abb346e3628ff564f
vsalter <vsalter1@student.gsu.edu>
2022-10-12 10:21:09 -0400

    form is now rendering

--- src/pages/NewPlanPage/NewPlanPage.jsx
+++ src/pages/NewPlanPage/NewPlanPage.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 
 export default function NewPlanPage() {
+  const [plan, setPlan] = useState({
+    name: "",
+    date: "",
+    startTime: "",
+    endTime: "",
+  });
+   
   return (
     <div>
       <h1>Create New Practice Plan</h1>
-      <div>
         <div className="form-container">
           <form>
             <label>Practice Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+            <input type="text" name="name" value={plan.name} required />
             <label>Practice Date</label>
-            <input type="date" name="date" value={this.state.confirm} onChange={this.handleChange} required />
+            <input type="text" name="date" value={plan.date}required />
             <label>Start Time</label>
-            <input type="timestamp" name="startTime" value={this.state.email} onChange={this.handleChange} required />
+            <input type="text" name="startTime" value={plan.startTime}required />
             <label>End Time</label>
-            <input type="timestamp" name="endTime" value={this.state.password} onChange={this.handleChange} required />
+            <input type="text" name="endTime" value={plan.endTime} required />
             <button type="submit">Create Practice Plan</button>
           </form>
         </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
-      </div>
+        {/* <p className="error-message">&nbsp;{this.state.error}</p> */}
     </div>
   );
 }
