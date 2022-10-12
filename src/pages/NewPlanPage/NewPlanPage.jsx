7757a154c8e3f2973616ac293ceac8f717a89ec1
vsalter <vsalter1@student.gsu.edu>
2022-10-12 09:44:39 -0400

    first attempt at adding form

--- src/pages/NewPlanPage/NewPlanPage.jsx
+++ src/pages/NewPlanPage/NewPlanPage.jsx
@@ -1,8 +1,27 @@
 import { useState } from 'react';
 
+
 export default function NewPlanPage() {
   return (
-    <h1>Create New Practice Plan</h1>
+    <div>
+      <h1>Create New Practice Plan</h1>
+      <div>
+        <div className="form-container">
+          <form>
+            <label>Practice Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+            <label>Practice Date</label>
+            <input type="date" name="date" value={this.state.confirm} onChange={this.handleChange} required />
+            <label>Start Time</label>
+            <input type="timestamp" name="startTime" value={this.state.email} onChange={this.handleChange} required />
+            <label>End Time</label>
+            <input type="timestamp" name="endTime" value={this.state.password} onChange={this.handleChange} required />
+            <button type="submit">Create Practice Plan</button>
+          </form>
+        </div>
+        <p className="error-message">&nbsp;{this.state.error}</p>
+      </div>
+    </div>
   );
 }
 
