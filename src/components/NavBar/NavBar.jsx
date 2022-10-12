9ae6516a6f3fc04b47b0574a5bdaa1eab782ea2c
vsalter <vsalter1@student.gsu.edu>
2022-10-11 22:40:59 -0400

    changed the links within the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,9 +10,9 @@ export default function NavBar({user, setUser}) {
 
     return (
         <nav>
-            <Link to="/orders">Order History</Link>
+            <Link to="/orders">View All Practice Plans</Link>
             &nbsp; | &nbsp;
-            <Link to="/orders/new">New Order</Link>
+            <Link to="/orders/new">New Plan</Link>
             &nbsp; | &nbsp;
             <span>Welcome, {user.name}</span>
             &nbsp;&nbsp; <Link to={""} onClick={handleLogOut}>Logout</Link>
