f89afdd637074b1f25daf31a34cd7e4ae2e2addb
vsalter <vsalter1@student.gsu.edu>
2022-10-12 17:57:29 -0400

    mounted practicePlan route in server.js

--- routes/api/practicePlan.js
+++ routes/api/practicePlan.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
+const practicePlanCtrl = require('../../controllers/api/practicePlan');
 
 // POST /api/users
-router.post('/', usersCtrl.create);
+router.post('/', practicePlanCtrl.create);
 
 module.exports = router;
